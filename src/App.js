d076673c30cafec0e6d11e1c042316ea424ff450
Trond Makonese <trondation@gmail.com>
2021-01-12 15:18:01 -0700

    Add icin images and style them to be the same size.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import image from './image-of-trond.jpeg'
+import trondImage from './image-of-trond.jpeg'
+import linkIcon from './linkDin.jpeg'
+import resumeIcon from './resume.jpeg'
+import gitIcon from './gitHub.png'
+import emailIcon from './email.jpeg'
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className='main-page'>
        <h1>Hello, I'm <span>Trond Makonese</span></h1>
-       <img src={image} alt="Photo of Trond" className="main_image"></img>
+       <img src={trondImage} alt="Photo of Trond" className="main_image"></img>
        <h1>A Front-End Web Developer</h1>
        <div className='social_wrapper'>
-        <a href="mailto:trondation@gmail.com">Email</a>
-        <a href="//github.com/trond240">Git Hub</a>
-        <a href="https://www.linkedin.com/in/trond-makonese-05610115a/">LinkDin</a>
-        <a href=''>Resume</a>
+        <a href="mailto:trondation@gmail.com"><img src={emailIcon} alt='email icon' className='icon_image'></img></a>
+        <a href="//github.com/trond240"><img src={gitIcon} alt='gitHub icon' className='icon_image'></img></a>
+        <a href="https://www.linkedin.com/in/trond-makonese-05610115a/"><img src={linkIcon} alt='linkDin icon' className='icon_image'></img></a>
+        <a href=''><img src={resumeIcon} alt='resume icon' className='icon_image'></img></a>
        </div>
       </header>
     </div>
