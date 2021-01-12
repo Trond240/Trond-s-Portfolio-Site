e9abddc037f5f8e92d52082d2096ad7e50776cc1
Trond Makonese <trondation@gmail.com>
2021-01-12 13:39:00 -0700

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='main-page'>
+       <h1>Hello, I'm <span>Trond Makonese</span></h1>
+       <h1>A Front-End Web Developer</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
