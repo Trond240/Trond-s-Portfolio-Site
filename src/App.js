e86b366796c6e745290a68c18ce346465e54a4de
Trond Makonese <trondation@gmail.com>
2021-01-12 13:57:46 -0700

    Basic information and links added to main page.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <header className='main-page'>
        <h1>Hello, I'm <span>Trond Makonese</span></h1>
        <h1>A Front-End Web Developer</h1>
+       <div className='social_wrapper'>
+        <a href="mailto:trondation@gmail.com">Email</a>
+        <a href="//github.com/trond240">Git Hub</a>
+        <a href="https://www.linkedin.com/in/trond-makonese-05610115a/">LinkDin</a>
+        <a href=''>Resume</a>
+       </div>
       </header>
     </div>
   );
