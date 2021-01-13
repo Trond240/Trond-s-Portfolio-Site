655b3c384b4aa07a14feb712570698e5d6e7e654
Trond Makonese <trondation@gmail.com>
2021-01-13 12:56:10 -0700

    Create about section, needs styling.

--- src/about/about.js
+++ src/about/about.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import trondImage from '../image-of-trond.jpeg'
 
 
 export const About = () => {
     return (
-        <section>
-            <h1>Made it</h1>
+        <section className='about_section'>
+            <div className='image_div'>
+                <img src={trondImage} alt="Photo of Trond" className="main_image"></img>
+            </div>
+            <div className='about_text_container'>
+                <h2 className='about_text'>Greetings!</h2>
+                <p className='about_text'>This will be some text about me and my journey with code</p>
+            </div>            
         </section>
     )
 }
