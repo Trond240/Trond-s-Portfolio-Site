40935bc1649f25a279dbe7882891ae6764264494
Trond Makonese <trondation@gmail.com>
2021-01-14 11:54:48 -0700

    Add footer/contact component to the app file.

--- src/App.js
+++ src/App.js
@@ -20,12 +20,12 @@ function App() {
         {/* <h1 className='hello'>Hello, I'm <span>Trond Makonese</span></h1> */}
         {/* <h1>A Front-End Web Developer</h1> */}
        </div>
-       <div className='social_wrapper'>
+       {/* <div className='social_wrapper'>
         <a href="mailto:trondation@gmail.com"><img src={emailIcon} alt='email icon' className='icon_image'></img></a>
         <a href="//github.com/trond240"><img src={gitIcon} alt='gitHub icon' className='icon_image'></img></a>
         <a href="https://www.linkedin.com/in/trond-makonese-05610115a/"><img src={linkIcon} alt='linkDin icon' className='icon_image'></img></a>
         <a href=''><img src={resumeIcon} alt='resume icon' className='icon_image'></img></a>
-       </div>
+       </div> */}
        <About />
        <Projects />
        <Contact />
