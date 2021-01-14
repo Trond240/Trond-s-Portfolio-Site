4550f2c4a4e7358a81f36c5c56850e538e343044
Trond Makonese <trondation@gmail.com>
2021-01-14 11:56:33 -0700

    Create contact component that handles as the page footer.

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -8,16 +8,15 @@ import emailIcon from '../email.jpeg'
 export const Contact = () => {
     return (
         <section className='contact_section'>
-            <i className='contact_message'>Please Feel Free to Contact me for work, suggestions, or networking</i>
-            <p>Trond Makonese</p>
-            <p>Denver, CO</p>
+            <h1 className='contact_message'>Please Feel Free to Contact me for work, suggestions, or networking</h1>
+            <h2>Trond Makonese</h2>
+            <h2>Denver, CO</h2>
             {/* <a rel="noopener noreferrer" aria-label="send email" href="mailto: trondation@gmail.com" target="_blank"><p class="about-text email-text">trondation@gmail.com</p></a> */}
             <div className='social_wrapper'>
                 <a href="mailto:trondation@gmail.com"><img src={emailIcon} alt='email icon' className='icon_image'></img></a>
                 <a href="//github.com/trond240"><img src={gitIcon} alt='gitHub icon' className='icon_image'></img></a>
                 <a href="https://www.linkedin.com/in/trond-makonese-05610115a/"><img src={linkIcon} alt='linkDin icon' className='icon_image'></img></a>
-                <a href=''><img src={resumeIcon} alt='resume icon' className='icon_image'></img></a>
-       </div>
+            </div>
         </section>
     )
 }
