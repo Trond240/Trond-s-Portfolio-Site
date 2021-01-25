1f311a7b0a36e12485bb12189107ae6af0e990be
Trond Makonese <trondation@gmail.com>
2021-01-25 09:27:46 -0700

    Create files for card game component.

---
+++ src/cardGame/cardGame.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class CardGame extends ReactComponent {
+    constructor() {
+        super();
+
+        
+    }
+
+    render() {
+        return (
+            <section className='game-section'>
+                <h1>Made it!</h1>
+            </section>
+        )
+    }
+}
+
+export default CardGame;
+
